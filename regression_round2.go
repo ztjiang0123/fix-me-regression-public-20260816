aae553c09e377c77436ce4dc2a6af77422032438
ZT <ztjiang@zts-macbook-pro.tailb998ad.ts.net>
2026-08-16 23:42:14 -0700

    test: add round-two Fix Me violations

---
+++ regression_round2.go
@@ -0,0 +1,17 @@
+package regression
+
+// This file intentionally contains analyzer violations used by the Fix Me
+// end-to-end regression suite. Each function has an excessive parameter list,
+// and their similar bodies exercise grouped fixes and multi-batch progress.
+func Round2Scenario01(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario02(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario03(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario04(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario05(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario06(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario07(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario08(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario09(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario10(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario11(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
+func Round2Scenario12(a, b, c, d, e, f, g int) int { return a + b + c + d + e + f + g }
